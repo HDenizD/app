84d410994ffe7f39c5c29036f58a440cdf2155a5
HDenizD <denizderezzed@gmail.com>
2024-04-02 17:29:42 +0200

    Update manifest.json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qb2l8an7sb"
+    "revision": "0.0q8dgl5tqco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
