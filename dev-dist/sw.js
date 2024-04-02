61245f93f5030d2ed2c39612f4fccab950ce50a1
HDenizD <denizderezzed@gmail.com>
2024-04-02 23:19:56 +0200

    Update service worker and manifest.json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0q8dgl5tqco"
+    "revision": "0.e96u7s2b7ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
